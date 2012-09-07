9c44816d6a1afaeaad88a9af5bda8df13bf5103a
Rafael Chacon <rafael@noomii.com>
2012-09-07 10:18:31 -0700

    Add colors to rake files

--- zoo/zoo-ruby.el
+++ zoo/zoo-ruby.el
@@ -1,6 +1,7 @@
 ;; on irb like shells, please handle properly the ansi escape codes
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '(".*\.rake$" . ruby-mode))
 (add-hook 'ruby-mode-hook (lambda ()
                             (ruby-end-mode 1)
                             (set (make-local-variable 'tab-width) 2)))
